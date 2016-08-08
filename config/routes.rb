4ead8cfc00bd6859ba64a00326fea45803a3c3de
Nawaz <nawazsherif97@gmail.com>
2016-08-08 21:02:17 +0530

    stylingv1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+	
 	root 'students#new'
-  resources :students
+  	resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
